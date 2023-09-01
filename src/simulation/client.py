f5ba8b00d12989c2264b9403fc4617b7a48cbcff
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2023-09-01 13:36:21 +0300

    added tcp server sample implementation

--- src/simulation/client.py
+++ src/simulation/client.py
@@ -7,7 +7,7 @@ client = ModbusTcpClient(SERVER_HOST, port=SERVER_PORT)
 
 
 def read_data(data_type, address, num_elements=1):
-    result = client.read_holding_registers(address, num_elements, unit=0x01)
+    result = client.read_holding_registers(address, num_elements, unit=0x00)
     if result.isError():
         print(f"Failed to read {data_type}. Error: {result}")
     else:
@@ -19,13 +19,13 @@ try:
 
     read_data("Holding Registers", address=0, num_elements=5)
 
-    address_to_write = 10
-    data_to_write = [100, 200, 300, 400]
-    response = client.write_registers(address_to_write, data_to_write, unit=0x01)
-    if response.isError():
-        print(f"Failed to write data. Error: {response}")
-
-    read_data("Holding Registers", address=0, num_elements=5)
+    # address_to_write = 10
+    # data_to_write = [100, 200, 300, 400]
+    # response = client.write_registers(address_to_write, data_to_write, unit=0x01)
+    # if response.isError():
+    #     print(f"Failed to write data. Error: {response}")
+    #
+    # read_data("Holding Registers", address=0x10, num_elements=5)
 
 finally:
     client.close()
