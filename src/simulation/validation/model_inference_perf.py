72bee1b1248ca332eaa97fe94bb3c26ac65d444e
Dunia Amin Mahboobeh <dam10098@hydra.abudhabi.nyu.edu>
2023-09-27 07:29:28 +0400

    train_test_sets

--- src/simulation/validation/model_inference_perf.py
+++ src/simulation/validation/model_inference_perf.py
@@ -7,15 +7,15 @@ from simplet5 import SimpleT5
 from torch.utils.benchmark import timer
 
 THREADS = 4
-MODELS = 2
-N = 20
+MODELS = 1
+N = 100
 
 
 def inference(model: SimpleT5, requests: DataFrame) -> [str]:
     predicted_responses = []
     for request in requests.iterrows():
         start = timer()
-        predicted_responses.append(model.predict(request[1]["request"])[0])
+        predicted_responses.append(model.predict(request[1]["source_text"])[0])
         end = timer()
         print(f"Thread: {threading.get_ident()}, Time: {end - start}", flush=True)
     return predicted_responses
@@ -23,11 +23,13 @@ def inference(model: SimpleT5, requests: DataFrame) -> [str]:
 
 def main():
     models = []
-    test_set: pd.DataFrame = pd.read_csv("./test_set.csv")
+    test_set: pd.DataFrame = pd.read_csv("/home/dam10098/ICSPot/finetuned_models/train_test_sets/test_set_100k_x.csv") #testset
 
     for _ in range(0, MODELS):
         model = SimpleT5()
-        model.load_model("byt5", "/Users/cv43/Downloads/finetuned/byt5", use_gpu=False)
+        model.load_model("byt5", "/home/dam10098/ICSPot/finetuned_models/outputs/simplet5-epoch-4-train-loss-0.4035-val-loss-0.3972", use_gpu=True)
+        # simplet5-epoch-4-train-loss-0.4064-val-loss-0.4183
+        # 
         models.append(model)
 
     try:
