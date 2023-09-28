a2fb346b4b9dbc74a54dae43a19f5c68852e1ad2
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2023-09-28 17:25:22 +0400

    updates in context dataset creation and enriched dataset with context.

--- src/simulation/dataset_generation/context_generation/context_dataset.py
+++ src/simulation/dataset_generation/context_generation/context_dataset.py
@@ -1,43 +1,11 @@
 import pandas as pd
-import numpy as np
 
-dataset = pd.read_csv("/home/dam10098/ICSPot/parsed_datasets/wago_context.csv")
+dataset = pd.read_csv("../../../../parsed_datasets/wago_context.csv")
 
-request_data = ""
-response_data = ""
-request = []
-response = []
-# request_data = []
-# response_data = []
-
-for i in range(len(dataset)):
-    if i == 0:
-        # request_data.append([dataset['source_text'][i], 'end/', ' ', 'end/', ' ', 'end/', ' ', 'end/', ' ']) #</s>
-        # response_data.append([dataset['target_text'][i]])
-        request_data  = f"{dataset['source_text'][i]}" 
-        response_data = f"{dataset['target_text'][i]}"
-        request.append(request_data)
-        response.append(response_data)
-
-    elif i == 1:
-        # request_data.append([dataset['source_text'][i-1], 'end/', dataset['target_text'][i-1], 'end/', dataset['source_text'][i], 'end/', ' ', 'end/', ' '])
-        # response_data.append([dataset['target_text'][i]])
-        request_data  = f"{dataset['source_text'][i-1]}{','}{dataset['target_text'][i-1]}{','}{dataset['source_text'][i]}" 
-        response_data = f"{dataset['target_text'][i]}"
-        request.append(request_data)
-        response.append(response_data)
-
-
-    elif i >= 2:
-        # request_data.append([dataset['source_text'][i-2], 'end/', dataset['target_text'][i-2], 'end/', dataset['source_text'][i-1], 'end/', dataset['target_text'][i-1], 'end/', dataset['source_text'][i]])
-        # response_data.append([dataset['target_text'][i]])
-        request_data  = f"{dataset['source_text'][i-2]}{','}{dataset['target_text'][i-2]}{','}{dataset['source_text'][i-1]}{','}{dataset['target_text'][i-1]}{','}{dataset['source_text'][i]}" 
-        response_data = f"{dataset['target_text'][i]}"
-        request.append(request_data)
-        response.append(response_data)
-
-# df = pd.DataFrame(list(zip(request_data,response_data)),columns =['source_text','target_text'])
-# df.to_csv('context_dataset_1.csv')
-
-df = pd.DataFrame(list(zip(request,response)),columns =['source_text','target_text'])
-df.to_csv('/home/dam10098/ICSPot/parsed_datasets/wago_context_X.csv')
+context = ""
+with open("./tmp.csv", "a+") as wago_context:
+    wago_context.write("source_text,target_text\n")
+    for i in range(0, len(dataset) - 2):
+        wago_context.write(f"{dataset['source_text'][i]}:{dataset['target_text'][i]}|"
+                           f"{dataset['source_text'][i + 1]}:{dataset['target_text'][i + 1]}|"
+                           f"{dataset['source_text'][i + 2]}:,{dataset['target_text'][i + 2]}" + "\n")
