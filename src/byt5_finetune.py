fe0e6631ec54d8cb6ea0263a33a70cf4fb3cff41
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2023-10-10 10:22:35 +0400

    refactoring phase 1.2

--- src/byt5_finetune.py
+++ src/byt5_finetune.py
@@ -9,18 +9,18 @@ from init import OUTPUTS_DIR, PROJECT_ROOT_DIR
 
 # 1:  byt5, 2: google/byt5-small or google/byt5-large, 3: read csv file, 4:epochs, 5:precision, 6: workers
 
-log = logger.setup_custom_logger(f"{sys.argv[2]}_{sys.argv[3]}_{sys.argv[4]}epochs_{sys.argv[5]}precision",
+log = logger.setup_custom_logger(f"{sys.argv[2]}_{sys.argv[3]}_epochs-{sys.argv[4]}_precision-{sys.argv[5]}",
                                  f"{OUTPUTS_DIR}/logs")
 
 
-def main():
+def finetune(model_type: str, model_name_path: str, model_name: str, csv_filename: str, epochs: int, precision: int):
     model = SimpleT5()
-    model.from_pretrained(f"{sys.argv[1]}", f"{sys.argv[2]}")
+    model.from_pretrained(model_type, f"{model_name_path}/{model_name}")
 
-    train_df = pd.read_csv(f"{OUTPUTS_DIR}/datasets/train/{sys.argv[3]}.csv")
+    train_df = pd.read_csv(f"{OUTPUTS_DIR}/datasets/train/{csv_filename}.csv")
     train_df = train_df[['source_text', 'target_text']]
 
-    val_df = pd.read_csv(f"{OUTPUTS_DIR}/datasets/validation/{sys.argv[3]}.csv")
+    val_df = pd.read_csv(f"{OUTPUTS_DIR}/datasets/validation/{csv_filename}.csv")
     val_df = val_df[['source_text', 'target_text']]
 
     model.train(train_df=train_df,
@@ -31,17 +31,28 @@ def main():
                 max_epochs=int(sys.argv[4]),
                 use_gpu=True,
                 dataloader_num_workers=int(sys.argv[6]),
-                outputdir=f"{PROJECT_ROOT_DIR}/models/{sys.argv[2]}_{sys.argv[3]}_{sys.argv[4]}epochs_{sys.argv[5]}precision_{datetime.datetime.now().strftime('%Y%m%dT%H%M')}",
+                outputdir=f"{PROJECT_ROOT_DIR}/models/{model_name}_{csv_filename}_epochs-{epochs}_precision-{precision}_{datetime.datetime.now().strftime('%Y%m%dT%H%M')}",
                 early_stopping_patience_epochs=0,
                 precision=int(sys.argv[5]),
                 save_only_last_epoch=True)
 
 
-if __name__ == '__main__':
+def main():
     try:
-        main()
+        model_type = sys.argv[1]
+        model_name_path = sys.argv[2]
+        model_name = sys.argv[3]
+        csv_filename = sys.argv[3]
+        epochs = int(sys.argv[4])
+        precision = int(sys.argv[5])
+
+        finetune(model_type, model_name_path, model_name, csv_filename, epochs, precision)
     except KeyboardInterrupt:
         log.error(traceback.format_exc())
         exit(1)
     except:
         log.error(traceback.format_exc())
+
+
+if __name__ == '__main__':
+    main()
