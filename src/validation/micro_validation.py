e3ff5eb98c85d58afd9262d217ebd5448dffd8e3
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2023-10-11 11:00:33 +0400

    refactor 5

--- src/validation/micro_validation.py
+++ src/validation/micro_validation.py
@@ -11,38 +11,47 @@ from mbtcp_validator import Validator
 from model.result import Result
 
 
-def validate(model: SimpleT5, tokenizer: ByT5Tokenizer, test_set: [], result_file: TextIO):
+def validate(model: SimpleT5, tokenizer: ByT5Tokenizer, test_set: [], result_file: TextIO, validation_type: str):
     for i in range(len(test_set)):
         to_save = Result()
+        context = ""
         request = test_set['request'][i]
+        response = ""
         expected_response = test_set['response'][i]
         try:
             if "|" in request:
                 question = request[request.rindex("|") + 1:len(request) - 1]
                 context = request[:request.rindex("|")]
-                to_save.context = context
                 inputs = model.tokenizer(question=question, context=context, return_tensors="pt")
                 output = tokenizer.decode(inputs['input_ids'][0])
-                predicted_response = model.predict(output)
+                response = model.predict(output)
             else:
                 question = request
-                predicted_response = model.predict(question)[0]
+                response = model.predict(question)[0]
 
-            validation = Validator(question, predicted_response)
-            validation.check_header_ids()
-            validation.check_payload()
+            validate_choice(validation_type, question, response, expected_response)
 
-            to_save.index = i
-            to_save.request = request
-            to_save.response = predicted_response
-            to_save.expected_response = expected_response
             to_save.valid = True
-
         except ValueError as exception:
             to_save.valid = False
             to_save.error = exception.__dict__
         finally:
+            to_save.index = i
+            to_save.context = context
+            to_save.request = request
+            to_save.response = response
+            to_save.expected_response = expected_response
             result_file.write(json.dumps(to_save.__dict__) + "\n")
+
+
+def validate_choice(validation_type: str, question: str, response: str, expected_response: str):
+    if validation_type == "micro":
+        validation = Validator(question, response)
+        validation.check_header_ids()
+        validation.check_payload()
+    else:
+        if response != expected_response:
+            raise ValueError("Not same as expected.")
 
 
 def main():
@@ -53,6 +62,7 @@ def main():
     parser.add_argument('-mnf', required=True)
     parser.add_argument('-ts', required=True)
     parser.add_argument('-g', default="False", required=False)
+    parser.add_argument('-val', default="micro", required=False)
     args = parser.parse_args()
 
     model_type = args.mt
@@ -61,6 +71,7 @@ def main():
     finetuned_model_name = args.mnf
     test_set_name = args.ts
     use_gpu = eval(args.g)
+    validation_type = args.val
 
     model = SimpleT5()
     model.load_model(f"{model_type}", f"{PROJECT_ROOT_DIR}/models/{finetuned_model_name}", use_gpu=use_gpu)
@@ -69,7 +80,7 @@ def main():
     test_set = test_set.rename(columns={'source_text': 'request', 'target_text': 'response'})
 
     with open(f"{OUTPUTS_DIR}/validation_data/{finetuned_model_name}.jsonl", "a") as result_file:
-        validate(model, tokenizer, test_set, result_file)
+        validate(model, tokenizer, test_set, result_file, validation_type)
 
 
 if __name__ == '__main__':
