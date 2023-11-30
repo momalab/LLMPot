fd233799f8c2f7dc154cef2069ab1aa1204b1d84
Dunia Amin Mahboobeh <dam10098@hydra.abudhabi.nyu.edu>
2023-11-30 15:48:37 +0400

    mbtcp serevr initial state

--- src/dataset_generation/mbtcp_server.py
+++ src/dataset_generation/mbtcp_server.py
@@ -8,10 +8,10 @@ from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 
 def start_server(address: str = "localhost", port: int = 502):
     store = ModbusSlaveContext(
-        di=ModbusSequentialDataBlock(0, [17] * 100),
-        co=ModbusSequentialDataBlock(0, [17] * 100),
-        hr=ModbusSequentialDataBlock(0, [17] * 100),
-        ir=ModbusSequentialDataBlock(0, [17] * 100))
+        di=ModbusSequentialDataBlock(0, [0] * 100),
+        co=ModbusSequentialDataBlock(0, [0] * 100),
+        hr=ModbusSequentialDataBlock(0, [0] * 100),
+        ir=ModbusSequentialDataBlock(0, [0] * 100))
 
     context = ModbusServerContext(slaves=store, single=True)
 
