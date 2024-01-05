6e38f0a37ba49a00ffbb4cd774db2476eb64049f
DuniaJMah <DuniaJMah@github.com>
2024-01-05 10:42:23 +0400

    rectified errors in the boundaries generation

--- src/dataset_generation/boundaries.py
+++ src/dataset_generation/boundaries.py
@@ -13,61 +13,64 @@ class MbtcpClient:
         address = [x for x in range(0, (samples_num+1))] #100+1=101 / addresses: 0 - 100
         num_elements = [z for z in range(1, (max_elements+1))] #3+1=4 / elements: 1,2,3
         boolean_value = 2
-        single_data_to_write = [y for y in range(0, single_data_value)] #10 / data: 0 -10
-        single_data_to_write = [max(single_data_to_write), int(statistics.mean(single_data_to_write)), min(single_data_to_write)]
         
-        for element in range(max_elements): 
+        single_data_to_write = [y for y in range(0, single_data_value)] #10 / data: 0 -10
+        values_without_boundaries = [val for val in single_data_to_write if val != min(single_data_to_write) and val != max(single_data_to_write)]
+        random_midpoint = random.choice(values_without_boundaries)
+        single_data_to_write = [max(single_data_to_write), random_midpoint, min(single_data_to_write)]
+            
+        if element == 0:
             values_combinations = itertools.product(range(multiple_data_value), repeat=num_elements[element])
             all_combinations = list(values_combinations)
-            
-            if element == 0:
-                min = all_combinations[0]
-                max = all_combinations[-1]
-                values = [x for x in all_combinations]
-                values_without_boundaries = [val for val in values if val != min_val and val != max_val]
-                random_midpoint = random.choice(values_without_boundaries)
-                subset_boundaries_Z = [min, random_midpoint, max]
-                            
-            
-            if element == 1:
-                subsets = [[(i, j) for j in range(multiple_data_value)] for i in range(multiple_data_value)]
-                X_groups = [[] for _ in range(int(len(all_combinations)/multiple_data_value))] #9/3 = 3
-                
-                for id, subset_x in enumerate(subsets):
-                    subset_id = (id // (multiple_data_value)) * (multiple_data_value) + id % (multiple_data_value)
-                    X_groups[subset_id].extend(subset_x)
-
-                subset_boundaries_X = [[] for _ in range(int(len(all_combinations)/multiple_data_value))]
-                for id, subset_x in enumerate(X_groups, start=1):
-                    values = [x for x in subset_x]
-                    min = subsets[id-1][0]
-                    max = subsets[id-1][-1]
-                    values_without_boundaries = [val for val in values if val != min and val != max]
-                    random_midpoint = random.choice(values_without_boundaries)  
-                    subset_boundaries_X[id-1] = [min, random_midpoint, max]
-
-
-            elif element == 2:
-                subset_groups = [[(i, j, k) for k in range(multiple_data_value)] for j in range(multiple_data_value) for i in range(multiple_data_value)]
-                Y_groups = [[] for _ in range(int(len(all_combinations)/multiple_data_value))] #27/3 = 9
-
-                for id, subset_y in enumerate(subset_groups):
-                    subset_id = (id // (multiple_data_value)) * (multiple_data_value) + id % (multiple_data_value)
-                    Y_groups[subset_id].extend(subset_y)
-
-                subset_boundaries_Y = [[] for _ in range(int(len(all_combinations)/multiple_data_value))]
-                for id, subset_y in enumerate(Y_groups, start=1):
-                    values = [x for x in subset_y]
-                    min = subset_groups[id-1][0]
-                    max = subset_groups[id-1][-1]
-                    values_without_boundaries = [val for val in values if val != min and val != max]
-                    random_midpoint = random.choice(values_without_boundaries) 
-                    subset_boundaries_Y[id-1] = [min, random_midpoint, max]
+            min_val = all_combinations[0]
+            max_val = all_combinations[-1]
+            values = [x for x in all_combinations]
+            values_without_boundaries = [val for val in values if val != min_val and val != max_val]
+            random_midpoint = random.choice(values_without_boundaries)
+            subset_boundaries_Z = [min_val, random_midpoint, max_val]
+            boundaries = subset_boundaries_Z
         
-        # Convert list of lists to a single list
-        subset_boundaries_X = [item for sublist in subset_boundaries_X for item in sublist]
-        subset_boundaries_Y = [item for sublist in subset_boundaries_Y for item in sublist]
-        boundaries = subset_boundaries_Z + subset_boundaries_X + subset_boundaries_Y
+        elif element == 1:
+            values_combinations = itertools.product(range(multiple_data_value), repeat=num_elements[element])
+            all_combinations = list(values_combinations)
+            subsets = [[(i, j) for j in range(multiple_data_value)] for i in range(multiple_data_value)]
+            X_groups = [[] for _ in range(int(len(all_combinations)/multiple_data_value))] #9/3 = 3
+            for id, subset_x in enumerate(subsets):
+                subset_id = (id // (multiple_data_value)) * (multiple_data_value) + id % (multiple_data_value)
+                X_groups[subset_id].extend(subset_x)
+
+            subset_boundaries_X = [[] for _ in range(int(len(all_combinations)/multiple_data_value))]
+            for id, subset_x in enumerate(X_groups, start=1):
+                values = [x for x in subset_x]
+                min_val = subsets[id-1][0]
+                max_val = subsets[id-1][-1]
+                values_without_boundaries = [val for val in values if val != min_val and val != max_val]
+                random_midpoint = random.choice(values_without_boundaries)  
+                subset_boundaries_X[id-1] = [min_val, random_midpoint, max_val]
+            
+            subset_boundaries_X = [item for sublist in subset_boundaries_X for item in sublist] # Convert list of lists to a single list
+            boundaries = subset_boundaries_X
+
+        elif element == 2:
+            values_combinations = itertools.product(range(multiple_data_value), repeat=num_elements[element])
+            all_combinations = list(values_combinations)
+            subset_groups = [[(i, j, k) for k in range(multiple_data_value)] for j in range(multiple_data_value) for i in range(multiple_data_value)]
+            Y_groups = [[] for _ in range(int(len(all_combinations)/multiple_data_value))] #27/3 = 9
+            for id, subset_y in enumerate(subset_groups):
+                subset_id = (id // (multiple_data_value)) * (multiple_data_value) + id % (multiple_data_value)
+                Y_groups[subset_id].extend(subset_y)
+
+            subset_boundaries_Y = [[] for _ in range(int(len(all_combinations)/multiple_data_value))]
+            for id, subset_y in enumerate(Y_groups, start=1):
+                values = [x for x in subset_y]
+                min_val = subset_groups[id-1][0]
+                max_val = subset_groups[id-1][-1]
+                values_without_boundaries = [val for val in values if val != min_val and val != max_val]
+                random_midpoint = random.choice(values_without_boundaries) 
+                subset_boundaries_Y[id-1] = [min_val, random_midpoint, max_val]
+            
+            subset_boundaries_Y = [item for sublist in subset_boundaries_Y for item in sublist] # Convert list of lists to a single list
+            boundaries = subset_boundaries_Y
         
         coils_combinations = itertools.product(range(boolean_value), repeat=num_elements[element]) 
         
@@ -89,7 +92,7 @@ class MbtcpClient:
             if (function_code == '3') or (function_code == '6'):
                 for element in range(1): #Explicitly for read/write single register
                     for sample in range(samples_num):
-                        for data in range(single_data_value):
+                        for data in range(3):
                             address, num_elements, single_data_to_write, boundaries, coils_combinations = self.generate_request(sample, samples_num, element, single_data_value, multiple_data_value, max_elements, data)      
                             self.mbtcp_requests.read_data("Read Single Registers", address, 3, num_elements=1)
                             self.mbtcp_requests.write_single_data("Write Single Registers", address, single_data_to_write, 6, value_to_write=0)
@@ -123,11 +126,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-ip', default="localhost", required=False)
     parser.add_argument('-p', default=502, required=False)
-    parser.add_argument('-num', default=100, required=False) 
-    parser.add_argument('-dat', default=10, required=False)
-    parser.add_argument('-mul', default=10, required=False)
-    parser.add_argument('-elem', default=3, required=False)
-    parser.add_argument('-fun', default='3', required=False) #example: -fun 1,3,etc change type to list #OR for single test default='16' and False and type int
+    parser.add_argument('-num', default=100, required=False) #100 addresses
+    parser.add_argument('-dat', default=10, required=False) #10
+    parser.add_argument('-mul', default=10, required=False) #10
+    parser.add_argument('-elem', default=3, required=False) #max 3
+    parser.add_argument('-fun', default='16', required=False) #example: -fun 1,3,etc change type to list #OR for single test default='16' and False and type int
     args = parser.parse_args()
 
     server_address = args.ip
