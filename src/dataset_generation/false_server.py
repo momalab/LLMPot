d8e669253993b87b0018e93487e3fa803e46f066
DuniaJMah <DuniaJMah@github.com>
2024-01-09 15:55:12 +0400

    deterministic dataset for incremental finetuning

---
+++ src/dataset_generation/false_server.py
@@ -0,0 +1,40 @@
+import sys
+
+from pymodbus.server import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.constants import ModbusStatus
+
+def start_server(address: str = "localhost", port: int = 502):
+    store = ModbusSlaveContext(
+        di=ModbusSequentialDataBlock(0, [10] * 151),#read coil
+        co=ModbusSequentialDataBlock(0, [10] * 153), #write coil
+        hr=ModbusSequentialDataBlock(0, [10] * 153), #write reg
+        ir=ModbusSequentialDataBlock(0, [10] * 151)) #read reg
+
+    context = ModbusServerContext(slaves=store, single=True)
+
+    identity = ModbusDeviceIdentification()
+    identity.VendorName = 'Pymodbus'
+    identity.ProductCode = 'PM'
+    identity.VendorUrl = 'http://github.com/riptideio/pymodbus/'
+    identity.ProductName = 'Pymodbus Server'
+    identity.ModelName = 'Pymodbus Server'
+    identity.MajorMinorRevision = '1.5'
+
+    print("Modbus Server started..")
+    StartTcpServer(context=context, identity=identity, address=(address, port))
+
+
+def main(argv: list[str]):
+    if len(argv) == 3:
+        server_address = str(argv[1])
+        server_port = int(argv[2])
+        start_server(server_address, server_port)
+    else:
+        start_server()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ src/dataset_generation/testing_outside_limits.py
@@ -0,0 +1,89 @@
+import argparse
+import itertools
+from mbtcp_requests import Mbtcp_Requests
+
+class MbtcpClient:
+
+    def __init__(self, server_address: str, server_port: int):
+        self.mbtcp_requests = Mbtcp_Requests(server_address, server_port)
+
+    @staticmethod
+    def generate_request(sample, element, multiple_data_value, data):
+        address = [x for x in range(101, 151)]
+        num_elements = [z for z in range(4, 10)]
+        boolean_value = 2
+        single_data_to_write = [y for y in range(10, 20)] 
+        values_combinations = itertools.product(range(multiple_data_value), repeat=num_elements[element])  
+        coils_combinations = itertools.product(range(boolean_value), repeat=num_elements[element])          
+        return address[sample], num_elements[element], single_data_to_write[data], values_combinations, coils_combinations
+    
+    def start_client(self, samples_num: int, single_data_value: int, multiple_data_value: int, max_elements: int, function_code: str):
+        self.mbtcp_requests.connect_client()
+        
+        try:
+            if (function_code == '1') or (function_code == '5'): 
+                for element in range(1): #Explicitly for read/write single coil
+                    for sample in range(samples_num):
+                        address, num_elements, single_data_to_write, values_combinations, coils_combinations = self.generate_request(sample, element, multiple_data_value, data=1)      
+                        for coil in range(2):
+                            value_to_write = [z for z in [True, False]]
+                            self.mbtcp_requests.read_data("Read Single Coil", address, 1, num_elements=1)
+                            self.mbtcp_requests.write_single_data("Write Single Coil", address, single_data_to_write, 5, value_to_write[coil])
+                
+            if (function_code == '3') or (function_code == '6'):
+                for element in range(1): #Explicitly for read/write single register
+                    for sample in range(samples_num):
+                        for data in range(single_data_value):
+                            address, num_elements, single_data_to_write, values_combinations, coils_combinations = self.generate_request(sample, element, multiple_data_value, data)      
+                            self.mbtcp_requests.read_data("Read Single Registers", address, 3, num_elements=1)
+                            self.mbtcp_requests.write_single_data("Write Single Registers", address, single_data_to_write, 6, value_to_write=0)
+                        self.mbtcp_requests.read_data("Read Single Registers", address, 3, num_elements=1)
+                
+            if function_code == '16':
+                for element in range(max_elements): #Covers 1, 2, and 3 registers requests
+                    for sample in range(samples_num): 
+                        address, num_elements, single_data_to_write, values_combinations, coils_combinations = self.generate_request(sample, element, multiple_data_value, data=1)      
+                        for values_to_write in values_combinations:
+                            self.mbtcp_requests.read_data("Read Multiple Registers", address, 3, num_elements)
+                            self.mbtcp_requests.write_multiple_data("Write Multiple Registers", address, values_to_write, 16)
+                        self.mbtcp_requests.read_data("Read Multiple Registers", address, 3, num_elements)
+
+            if function_code == '15':
+                for element in range(max_elements): #Covers single, 2, and 3 coils requests
+                    for sample in range(samples_num):
+                        address, num_elements, single_data_to_write, values_combinations, coils_combinations = self.generate_request(sample, element, multiple_data_value, data=1)      
+                        for coil_value in coils_combinations:
+                            self.mbtcp_requests.read_data("Read Multiple Coils", address, 1, num_elements)
+                            self.mbtcp_requests.write_multiple_data("Write Multiple Coils", address, coil_value, 15)
+
+        except KeyboardInterrupt:
+            print("Client stopped by user.")
+        finally:
+            self.mbtcp_requests.close_client()
+
+def main():
+    def list_of_strings(arg):
+        return arg.split(',')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', default="localhost", required=False)
+    parser.add_argument('-p', default=502, required=False)
+    parser.add_argument('-num', default=50, required=False) 
+    parser.add_argument('-dat', default=10, required=False)
+    parser.add_argument('-mul', default=6, required=False)
+    parser.add_argument('-elem', default=6, required=False)
+    parser.add_argument('-fun', default='3', required=False) #example: -fun 1,3,etc change type to list #OR for single test default='16' and False and type int
+    args = parser.parse_args()
+
+    server_address = args.ip
+    server_port = int(args.p)
+    samples_num = int(args.num) #50 addresses (101-150)> use false_server.py 
+    single_data_value = int(args.dat) #values 10 to 20
+    multiple_data_value = int(args.mul) 
+    max_elements = int(args.elem) #upto 6 registers per request (4-9)
+    function_code = args.fun #example: -fun 1,3
+    
+    mbtcp_client = MbtcpClient(server_address, server_port)
+    mbtcp_client.start_client(samples_num, single_data_value, multiple_data_value, max_elements, function_code)
+
+if __name__ == '__main__':
+    main()
