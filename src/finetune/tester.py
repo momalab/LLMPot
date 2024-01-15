ff58e3273d7d778a68e9bc6a391917c0b93a9afe
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2024-01-15 16:46:22 +0200

    finetune over finetune

--- src/finetune/tester.py
+++ src/finetune/tester.py
@@ -55,8 +55,6 @@ def main():
         if dataset.column_names.keys().__contains__("Unnamed: 0"):
             dataset = dataset.remove_columns("Unnamed: 0")
 
-        dataset = dataset.remove_columns("Unnamed: 0")
-
         dataloader = DataLoader(dataset["test"], batch_size=10, shuffle=True, num_workers=1)
         trainer.test(model=model, dataloaders=dataloader)
 
