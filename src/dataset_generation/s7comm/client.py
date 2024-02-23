f1c536719216e49272cfac1131aa66013cfc94ac
DuniaJMah <DuniaJMah@github.com>
2024-02-23 16:39:53 +0400

    s7comm final refactoring

--- src/dataset_generation/s7comm/client.py
+++ src/dataset_generation/s7comm/client.py
@@ -1,11 +1,8 @@
 import argparse
-import random
 from typing import Tuple
 from snap7.client import Client
 from snap7.types import Areas
 from snap7.util import get_word, get_bool
-
-from dataset_generation.s7comm.invalid_function import S7_CustomInvalidFunctionRequest
 
 
 class S7Client(Client):
@@ -16,13 +13,6 @@ class S7Client(Client):
         self.port = port
         self.connect(self.ip, 0, 0, self.port)
         self.get_connected()
-
-    def illegal_function(self):
-        valid_function_code = [240, 241, 242, 243, 244, 245, 246] # Read = 0x04 and Write = 0x05 - must be altered
-        false_function_code = random.choice([x for x in range(0, 255) if x not in valid_function_code])
-        print(f"False FC is: {false_function_code}")
-        request = S7_CustomInvalidFunctionRequest(false_function_code, self.ip, self.port)
-        return request.send_custom_s7_command()
 
     def func_wrapper(self, func: callable, *args):
         try:
