c7fe22e87c88740c42f9c7aec2826d345df185d9
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2024-02-26 17:17:11 +0400

    add top_k=2 to save good result models

--- src/finetune/finetuner.py
+++ src/finetune/finetuner.py
@@ -109,7 +109,7 @@ class Finetuner:
             checkpoint_callback = ModelCheckpoint(
                 monitor=self._val_loss_const,
                 filename=finetuner_model.dataset_filename,
-                save_top_k=0,
+                save_top_k=2,
                 save_last=True,
                 mode='min',
                 auto_insert_metric_name=False
