ee6b7dcd80d9bbdd4e8e4e230143fb2e1cdecc67
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2024-03-22 10:48:59 +0400

    parser ignore keys not found in responses

--- src/dataset_generation/parse.py
+++ src/dataset_generation/parse.py
@@ -11,18 +11,18 @@ SOURCE_TEXT = "source_text"
 TARGET_TEXT = "target_text"
 
 
-def parse_with_file(protocol: str, capture_layer: str, port: int, pcap: str, csv: str, context_length: int, enc_type: str = "str"):
+def parse_with_file(protocol: str, capture_layer: str, port: int, pcap: str, csv: str, context_length: int):
     cap = pyshark.FileCapture(f"{DATASET_DUMPS}/{pcap}.pcap", use_json=True, include_raw=True,
                               display_filter=f'{protocol}',
                               decode_as={f'tcp.port=={port}': f'{capture_layer}'})
-    __parse(protocol, port, cap, csv, context_length, enc_type)
+    __parse(protocol, port, cap, csv, context_length)
 
 
-def parse_without_file(protocol: str, port: int, cap, csv_filename: str, context_length: int, enc_type: str = "str"):
-    __parse(protocol, port, cap, csv_filename, context_length, enc_type)
+def parse_without_file(protocol: str, port: int, cap, csv_filename: str, context_length: int):
+    __parse(protocol, port, cap, csv_filename, context_length)
 
 
-def __parse(protocol: str, port: int, cap, csv_filename: str, context_length: int, enc_type: str):
+def __parse(protocol: str, port: int, cap, csv_filename: str, context_length: int):
     dataset_dict = {SOURCE_TEXT: [], TARGET_TEXT: []}
     request_packets: {int: str} = dict()
     response_packets: {int: str} = dict()
@@ -44,7 +44,11 @@ def __parse(protocol: str, port: int, cap, csv_filename: str, context_length: in
         if int(packet.tcp.dstport) == port:
             request_packets[int(packet.tcp.ack_raw[-1])] = the_value
         else:
-            response_packets[int(packet.tcp.seq_raw[-1])] = the_value
+            try:
+                response_packets[int(packet.tcp.seq_raw[-1])] = the_value
+            except:
+                print("Error: ", packet.tcp.seq_raw)
+                response_packets.pop(int(packet.tcp.seq_raw))
 
     for tid, entry in tqdm(request_packets.items()):
         dataset_dict[SOURCE_TEXT].append(entry)
@@ -66,13 +70,12 @@ def __parse(protocol: str, port: int, cap, csv_filename: str, context_length: in
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-pcap', default="lala", required=False)
+    parser.add_argument('-pcap', default="chemical-1200", required=False)
     parser.add_argument('-csv', default="lala", required=False)
     parser.add_argument('-p', default="502", required=False)
     parser.add_argument('-layer', default="mbtcp", required=False)
     parser.add_argument('-pr', default="mbtcp", required=False)
     parser.add_argument('-clen', default=2, required=False)
-    parser.add_argument('-f', default="str", required=False)
     args = parser.parse_args()
 
     pcap = args.pcap
@@ -81,9 +84,8 @@ def main():
     capture_layer = args.layer
     protocol = args.pr
     context_length = int(args.clen)
-    enc_type = args.f
 
-    parse_with_file(protocol, capture_layer, port, pcap, csv, context_length, enc_type)
+    parse_with_file(protocol, capture_layer, port, pcap, csv, context_length)
 
 
 if __name__ == '__main__':
