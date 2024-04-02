31a746f8492eb5988a6ef3ca00ab7401012d4894
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2024-04-02 22:48:27 +0400

    test datasets

--- src/finetune/tester.py
+++ src/finetune/tester.py
@@ -28,7 +28,7 @@ def main():
         config = json.loads(config)
         finetuner_model = FinetunerModel(**config)
         finetuner_model.experiment = experiment
-
+        finetuner_model.current_dataset = finetuner_model.test
         new_datetime = finetuner_model.start_datetime
         finetuner_model.start_datetime = os.listdir(f"{CHECKPOINTS}/{finetuner_model.experiment_filename}/{finetuner_model.the_name}")[0]
 
