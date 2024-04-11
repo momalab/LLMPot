cff004c13b780703da3a74c377b05c774aa40f45
Christoforos Vasilatos <christoforos.vasilatos@gmail.com>
2024-04-11 13:40:17 +0400

    s7comm additional datasets

--- src/plots/from_csv.py
+++ src/plots/from_csv.py
@@ -12,12 +12,12 @@ from finetune.model.finetuner_model import FinetunerModel
 
 
 FONT_FAMILY = "Times New Roman"
+SYMBOL = ['cross', 'diamond-open']
 
 
 class Plots:
     def __init__(self, experiment: str):
-        self._metric = 'csv-accuracy/micro'
-        symbol = ['circle-open', 'circle', 'circle-open-dot', 'square']
+        self._metric = 'csv-accuracy/validator'
         self._finetuner = self._load_experiment(experiment)
 
         self._protocol = set(map(lambda x: x.protocol, self._finetuner.datasets)).pop()
@@ -28,7 +28,11 @@ class Plots:
 
         palette = qualitative.Alphabet
         self._color_map = {dataset.size: palette[i] for i, dataset in enumerate(self._finetuner.datasets)}
-        self._symbol_map = {functions_set: symbol[i] for i, functions_set in enumerate(self._functions_set)}
+        self._symbol_map = {functions_set: SYMBOL[i] for i, functions_set in enumerate(self._functions_set)}
+
+    @staticmethod
+    def get_symbol(key: str, keys: [], options: []):
+        return {key: options[i] for i, key in enumerate(keys)}[key]
 
     @staticmethod
     def darken_hex_color(hex_color, reduction_percentage=30):
@@ -53,27 +57,31 @@ class Plots:
     def accuracy_per_epoch(self):
         fig = go.Figure()
         for dataset in self._finetuner.datasets:
+            if not os.path.exists(f"{CHECKPOINTS}/{dataset}_metrics.csv"):
+                continue
+
             df = pd.read_csv(f"{CHECKPOINTS}/{dataset}_metrics.csv")
             df = df.drop(columns=['csv-val_loss_step', 'csv-val_loss_epoch', 'csv-train_loss_step', 'csv-train_loss_epoch'])
             df = df.dropna()
 
             fig.add_trace(go.Scatter(x=df['csv-epoch'], y=df[self._metric],
                                      mode='lines+markers',
-                                     name=f"{dataset.size}",
+                                     name=f"{dataset.size}-{dataset.functions_str(',')}",
                                      line=dict(width=1.5, color=self._color_map[dataset.size], shape='spline'),
                                      marker=dict(size=6, symbol=self._symbol_map[dataset.functions_str(",")])))
 
         fig.update_layout(
-            title=f'Protocol: {self._protocol}, Model: {self._finetuner.model_name}, Validation: {self._metric.split("/")[1]}',
+            # title=f'Protocol: {self._protocol}, Model: {self._finetuner.model_name}, Validation: {self._metric.split("/")[1]}',
             title_font_size=28,
             xaxis_title='Epoch',
             yaxis_title='Accuracy',
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
-            margin=dict(l=0, r=0, b=0, t=60, pad=0),
+            height=1200,
+            margin=dict(l=0, r=0, b=0, pad=0),
             font=dict(family=FONT_FAMILY, size=26, color="Black"),
-            legend=dict(yanchor="bottom", y=0.02, xanchor="right", x=0.9,
-                        font=dict(family=FONT_FAMILY, size=26, color="black")),
+            legend=dict(yanchor="bottom", y=1, xanchor="right", x=1, orientation='h',
+                        font=dict(family=FONT_FAMILY, size=26)),
             xaxis=dict(showgrid=False, showline=False),
             yaxis=dict(showgrid=False, showline=False, range=[0.3, 1.05])
         )
@@ -85,11 +93,12 @@ class Plots:
 
     def loss_per_epoch(self):
         fig = go.Figure()
-        palette = qualitative.Alphabet
-        self._color_map = {dataset.size: palette[i] for i, dataset in enumerate(self._finetuner.datasets)}
+        self._symbol_map = {name: SYMBOL[i] for i, name in enumerate(['train', 'val'])}
         for dataset in self._finetuner.datasets:
+            if not os.path.exists(f"{CHECKPOINTS}/{dataset}_metrics.csv"):
+                continue
             df = pd.read_csv(f"{CHECKPOINTS}/{dataset}_metrics.csv")
-            df.drop(columns=['csv-val_loss_step', 'csv-train_loss_step', 'csv-accuracy/micro', 'csv-accuracy/none', 'step'], inplace=True)
+            df.drop(columns=['csv-val_loss_step', 'csv-train_loss_step', 'csv-accuracy/validator', 'csv-accuracy/exact', 'step'], inplace=True)
 
             val_df = df.dropna(subset=["csv-val_loss_epoch"]).drop(columns=["csv-train_loss_epoch"])
             train_df = df.dropna(subset=["csv-train_loss_epoch"]).drop(columns=["csv-val_loss_epoch"])
@@ -98,28 +107,30 @@ class Plots:
 
             fig.add_trace(go.Scatter(x=df['csv-epoch'], y=df['csv-val_loss_epoch'],
                                      mode='lines+markers',
-                                     name=f"val-{dataset.size}",
-                                     line=dict(width=1.5, color=self.darken_hex_color(self._color_map[dataset.size])),
-                                     marker=dict(size=6, symbol=self._symbol_map[dataset.functions_str(",")])))
+                                     name=f"val-{dataset.size}-{dataset.functions_str(',')}",
+                                     line=dict(width=1.5, color=self.darken_hex_color(self._color_map[dataset.size]), shape='spline'),
+                                     marker=dict(size=6, symbol=self.get_symbol('val', ['train', 'val'], SYMBOL))))
 
             fig.add_trace(go.Scatter(x=df['csv-epoch'], y=df['csv-train_loss_epoch'],
                                      mode='lines+markers',
-                                     name=f"train-{dataset.size}",
+                                     name=f"train-{dataset.size}-{dataset.functions_str(',')}",
                                      line=dict(width=1.5, color=self._color_map[dataset.size], shape='spline'),
-                                     marker=dict(size=6, symbol=self._symbol_map[dataset.functions_str(",")])))
+                                     marker=dict(size=6, symbol=self.get_symbol('train', ['train', 'val'], SYMBOL))))
 
         fig.update_layout(
-            title=f'Protocol: {self._protocol}, Model: {self._finetuner.model_name}, Validation: {self._metric.split("/")[1]}',
+            # title=f'Train/Validation Loss - Protocol: {self._protocol}, Model: {self._finetuner.model_name}',
             title_font_size=28,
             yaxis_type='log',
             xaxis_title='Epoch',
-            yaxis_title='Accuracy',
+            yaxis_title='Loss',
+            height=1200,
+            autosize=False,
             paper_bgcolor='rgba(0,0,0,0)',
             plot_bgcolor='rgba(0,0,0,0)',
-            margin=dict(l=0, r=0, b=0, t=60, pad=0),
+            margin=dict(l=0, r=0, b=0, pad=0),
             font=dict(family=FONT_FAMILY, size=26, color="Black"),
-            legend=dict(yanchor="bottom", y=0.4, xanchor="right", x=0.8,
-                        font=dict(family=FONT_FAMILY, size=26, color="black")),
+            legend=dict(yanchor="bottom", y=1, xanchor="right", x=1, orientation='h',
+                        font=dict(family=FONT_FAMILY, size=26)),
             xaxis=dict(showgrid=False, showline=False),
             yaxis=dict(showgrid=False, showline=False)
         )
@@ -127,11 +138,13 @@ class Plots:
         fig.show()
 
         os.makedirs(f"{ASSETS}/{self._finetuner.experiment}/", exist_ok=True)
-        fig.write_image(f"{ASSETS}/{self._finetuner.experiment}/{self._validation_type}.png")
+        fig.write_image(f"{ASSETS}/{self._finetuner.experiment}/losses.png")
 
     def barchart_best_accuracy_of_each(self):
         df_plot = pd.DataFrame(columns=['size', 'accuracy', 'functions'])
         for index, dataset in enumerate(self._finetuner.datasets):
+            if not os.path.exists(f"{CHECKPOINTS}/{dataset}_metrics.csv"):
+                continue
             df = pd.read_csv(f"{CHECKPOINTS}/{dataset}_metrics.csv")
             df = df.drop(columns=['csv-val_loss_step', 'csv-val_loss_epoch', 'csv-train_loss_step', 'csv-train_loss_epoch'])
             df = df.dropna()
@@ -163,7 +176,12 @@ class Plots:
 
 
 if __name__ == '__main__':
+    plot = Plots("s7comm-protocol-emulation.json")
+    plot.accuracy_per_epoch()
+    plot.loss_per_epoch()
+    # plot.barchart_best_accuracy_of_each()
+
     plot = Plots("mbtcp-protocol-emulation.json")
-    # plot.accuracy_per_epoch()
+    plot.accuracy_per_epoch()
     plot.loss_per_epoch()
     # plot.barchart_best_accuracy_of_each()
