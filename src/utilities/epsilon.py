608ea09cc71235296a480c29ce8e359888807a5a
Christoforos Vasilatos <cv43@nyu.edu>
2024-04-28 14:12:38 +0400

    fixes icspatch and other

--- src/utilities/epsilon.py
+++ src/utilities/epsilon.py
@@ -8,17 +8,17 @@ from typing import Tuple
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 
-def calculate_error_margin(path: str, file: str) -> Tuple[float, float, float]:
+def calculate_error_margin(path: str, file: str, registers: int) -> Tuple[float, float, float]:
     with open(f"{path}/{file}.jsonl", "r") as data:
         data = [json.loads(line) for line in data]
 
-    return calculate(pd.DataFrame(data), path, file)
+    return calculate(pd.DataFrame(data), path, file, registers)
 
-def calculate(df: pd.DataFrame, path, file):
+def calculate(df: pd.DataFrame, path: str, file: str, registers: int) -> Tuple[float, float, float]:
 
     results_data = []
     for _, row in df.iterrows():
-        distance = -1
+        distance = "Invalid"
         percentage = -1
         try:
             context: str = row['context']
@@ -28,58 +28,65 @@ def calculate(df: pd.DataFrame, path, file):
 
             bytes.fromhex(response) # just check if it is a valid packet
 
-            context = context.split(":")[0]
-            i1 = int(context[-8:][0:4], 16)
-            i2 = int(context[-8:][4:8], 16)
+            reg_values = context.split(":")[0][-4*registers:]
+            context_chunks = [reg_values[i:i + 4] for i in range(0, len(reg_values), 4)]
+            context_chunks = [int(chunk, 16) for chunk in context_chunks]
 
-            r1 = int(response[-8:][0:4], 16)
-            r2 = int(response[-8:][4:8], 16)
+            reg_values = response[-4*registers:]
+            response_chunks = [reg_values[i:i + 4] for i in range(0, len(reg_values), 4)]
+            response_chunks = [int(chunk, 16) for chunk in response_chunks]
 
-            er1 = int(expected_response[-8:][0:4], 16)
-            er2 = int(expected_response[-8:][4:8], 16)
+            reg_values = expected_response[-4*registers:]
+            e_response_chunks = [reg_values[i:i + 4] for i in range(0, len(reg_values), 4)]
+            e_response_chunks = [int(chunk, 16) for chunk in e_response_chunks]
 
-            i_dec = BinaryPayloadDecoder.fromRegisters([i1, i2], Endian.BIG, wordorder=Endian.LITTLE)
-            r_dec = BinaryPayloadDecoder.fromRegisters([r1, r2], Endian.BIG, wordorder=Endian.LITTLE)
-            er_dec = BinaryPayloadDecoder.fromRegisters([er1, er2], Endian.BIG, wordorder=Endian.LITTLE)
+            x = BinaryPayloadDecoder.fromRegisters(context_chunks, Endian.BIG, wordorder=Endian.LITTLE)
+            y = BinaryPayloadDecoder.fromRegisters(response_chunks, Endian.BIG, wordorder=Endian.LITTLE)
+            y_orig = BinaryPayloadDecoder.fromRegisters(e_response_chunks, Endian.BIG, wordorder=Endian.LITTLE)
 
-            i_dec = i_dec.decode_32bit_float()
-            r_dec = r_dec.decode_32bit_float()
-            er_dec = er_dec.decode_32bit_float()
+            x = x.decode_32bit_float()
+            y = y.decode_32bit_float()
+            y_orig = y_orig.decode_32bit_float()
 
-            distance = abs(r_dec - er_dec)
-            percentage = distance / er_dec
+            distance = abs(y - y_orig)
+            if y_orig != 0:
+                percentage = distance / y_orig
+            elif y != 0:
+                percentage = distance / y
+            else:
+                percentage = 0
         except:
             traceback.print_exc()
             pass
         finally:
             results_data.append({
-                'x': i_dec,
-                'y': r_dec,
-                'y_orig': er_dec,
+                'context': context,
                 'request': request,
                 'response': response,
                 'expected_response': expected_response,
                 'distance': distance,
+                'x': x,
+                'y': y,
+                'y_orig': y_orig,
                 'percentage': percentage})
 
 
     results = pd.DataFrame(results_data)
     results.to_json(f"{path}/epsilon-{file}.jsonl", orient='records', lines=True)
+    results.query(f"distance != 'Invalid'", inplace=True)
     mean_value = results['distance'].sum() / len(results['distance'])
     std_dev = results['distance'].std()
     mean_percentage = results['percentage'].mean()
 
-    print(results['distance'])
-    print(results['distance'].sum())
 
-    for _, row in results.iterrows():
-        if row['distance'] > 0.1:
-            print(f"X: {row['x']}, Y: {row['y']}, Y_orig: {row['y_orig']}, distance: {row['distance']}, percentage: {row['percentage']}")
+    # for _, row in results.iterrows():
+    #     if row['distance'] > 0.1:
+    #         print(f"X: {row['x']}, Y: {row['y']}, Y_orig: {row['y_orig']}, distance: {row['distance']}, percentage: {row['percentage']}")
 
-    print(f"Mean: {mean_value}, std: {std_dev}, mean_percentage: {mean_percentage}")
+    print(f"Mean: {round(mean_value, 4)}, std: {round(std_dev, 4)}, mean_percentage: {mean_percentage}")
 
     return mean_value, std_dev, mean_percentage
 
 
 if __name__ == "__main__":
-    calculate_error_margin("/media/shared/dam10098/ICSPot/checkpoints/mbtcp-icspatch-processes.json/mbtcp-aircraft-c1-s4096/20240427T1142/", "epoch-13_val_type-validator")
+    calculate_error_margin("/media/shared/dam10098/ICSPot/checkpoints/mbtcp-icspatch-processes.json/mbtcp-aircraft-c1-s4096/20240427T1142/", "epoch-13_val_type-validator", 2)
