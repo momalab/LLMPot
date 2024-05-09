acb2caf0ab74611d80948075e527883c38aaba22
Christoforos Vasilatos <cv43@nyu.edu>
2024-05-09 15:06:31 +0400

    added all results except the model checkpoints (ckpt)

---
+++ checkpoints/mbtcp-protocol-emulation.json.old_new/download_metrics.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "Enter SSH Password: "
+read -s SSH_PASS
+
+find . -name metrics.csv -type f | while read -r file; do
+    parent_dir=$(dirname "$file")
+        grandparent_dir_name=$(basename "$(dirname "$parent_dir")")
+	    echo "Copying $file to $1:/Users/cv43/nyuad/source/ICSPot/checkpoints/${grandparent_dir_name}_metrics.csv"
+	        sshpass -p "$SSH_PASS" scp "$file" "cv43@$1:/Users/cv43/nyuad/source/ICSPot/checkpoints/${grandparent_dir_name}_metrics.csv"
+	done
